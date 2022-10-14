f0c7ff690af8d5599fdb25085c26bbba4c14569d
dk19999 <rattandeepak1999@gmail.com>
2022-10-14 15:21:28 +0530

    feat: add delete , update and getSingleTodo API routes

--- src/pages/api/todo/[id].tsx
+++ src/pages/api/todo/[id].tsx
@@ -2,22 +2,70 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const requestMethod = req.method;
-  const body = JSON.parse(req.body);
-  const { id } = req.query
-  switch (requestMethod) {
-    case 'GET':
-      //get a particular todo with id 
-      return res.status(200).json({ message: `You submitted the following data: ${body}` })
-    // handle other HTTP methods
-    case 'UPDATE':
-      //update todo
-      return res.status(200).json({ message: `You submitted the following data: ${body}` })
-    case 'DELETE':
-      //delete todo
-      return  res.status(200).json({ message: `You submitted the following data: ${body}` })
-    default:
-      return res.status(200).json({ message: 'Welcome to API Routes!'})
+import apiHandler from '@/utils';
+
+import TodoModel, { ITodo } from '../../../models/todo';
+
+const getTodo = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query;
+    const todo = await TodoModel.findOne({ _id: id });
+    if (!todo) {
+      res.status(404).json({ message: `No todo found with id ${id} ` });
+      return;
+    }
+    res.status(200).json({ todo });
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error, Something went wrong!' });
   }
-}
+};
+
+const updateTodo = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query;
+    const { name, completed }: ITodo = req.body;
+
+    if (!name || completed === undefined) {
+      return res.status(400).json({ message: 'Bad Request' });
+    }
+
+    const todo = await TodoModel.findOneAndUpdate(
+      { _id: id },
+      { name, completed }
+    );
+    if (!todo) {
+      return res.status(404).json({ message: `No todo found with id ${id}` });
+    }
+    res.status(200).json({ message: 'To do updated successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error, Something went wrong!' });
+  }
+};
+
+const deleteTodo = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query;
+    const { name, completed }: ITodo = req.body;
+
+    if (!name || completed === undefined) {
+      return res.status(400).json({ message: 'Bad Request' });
+    }
+
+    const todo = await TodoModel.findOneAndDelete(
+      { _id: id },
+      { name, completed }
+    );
+    if (!todo) {
+      return res.status(404).json({ message: `No todo found with id ${id}` });
+    }
+    res.status(200).json({ message: 'To do deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error, Something went wrong!' });
+  }
+};
+
+export default apiHandler({
+  get: getTodo,
+  put: updateTodo,
+  delete: deleteTodo,
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,20 +4,22 @@ import dbConnect from '@/lib/db-connect';
 
 type httpMethod = 'get' | 'put' | 'post' | 'put' | 'delete' | 'patch';
 
-type RequireAtLeastOne<T, Keys extends keyof T = keyof T> =
-    Pick<T, Exclude<keyof T, Keys>>
-    & {
-        [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
-    }[Keys]
+type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+  }[Keys];
 
 type handlers = {
-  [key in  httpMethod]: (
+  [key in httpMethod]: (
     req: NextApiRequest,
     res: NextApiResponse
   ) => Promise<void>;
 };
 
-type handler = RequireAtLeastOne<handlers , httpMethod >
+type handler = RequireAtLeastOne<handlers, httpMethod>;
 
 export default function apiHandler(handler: handler) {
   return async (req: NextApiRequest, res: NextApiResponse) => {
@@ -39,6 +41,7 @@ export default function apiHandler(handler: handler) {
       // route handler
       await handler[method]?.(req, res);
     } catch (err) {
+      console.log(err, 'err');
       // global error handler
       // errorHandler(err, res);
     }
