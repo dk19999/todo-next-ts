250a273b664ac43bc5b6f64e9b6f77ed8ae1f9a7
dk19999 <rattandeepak1999@gmail.com>
2022-10-18 23:51:18 +0530

    feat:add service file for client side api calls

---
+++ src/services/api/todo.ts
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+export const getTodos = (
+  callback: (toDoList: ToDoList) => void,
+  errorCallback: (error: Error) => void
+) => {
+  axios({ url: '/api/todos' })
+    .then((res) => {
+      callback && callback(res.data.todos);
+    })
+    .catch((error) => {
+      errorCallback && errorCallback(error);
+    });
+};
+
+export const createTodo = (
+  data: Omit<ToDoListItem, '_id'>,
+  callback: (toDo?: ToDoListItem ) => void,
+  errorCallback: (error: Error) => void
+) => {
+  axios({ url: '/api/todos', method: 'post', data })
+    .then((res) => {
+      callback && callback(res.data);
+    })
+    .catch((error) => {
+      errorCallback && errorCallback(error);
+    });
+};
+
+export const updateTodo = (
+  data: ToDoListItem,
+  callback: (data: { message: string }) => void,
+  errorCallback: (error: Error) => void
+) => {
+  axios({ url: `/api/todos/${data._id}`, data, method: 'put' })
+    .then((res) => {
+      callback && callback(res.data);
+      console.log('🚀 ~ file: todo.ts ~ line 33 ~ .then ~ res.data', res.data);
+    })
+    .catch((error) => {
+      errorCallback && errorCallback(error);
+    });
+};
+
+export const deleteTodo = (
+  id: string,
+  callback: (data: { message: string }) => void,
+  errorCallback: (error: Error) => void
+) => {
+  axios({ url: `api/todos/${id}`, method:'delete' })
+    .then((res) => {
+      callback && callback(res.data);
+      console.log('🚀 ~ file: todo.ts ~ line 33 ~ .then ~ res.data', res.data);
+    })
+    .catch((error) => {
+      errorCallback && errorCallback(error);
+    });
+};
